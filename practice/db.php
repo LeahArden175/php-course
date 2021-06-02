6de6b302c47709afe5a906a9aa1a8743223331e5
Leah Arden <leaharden175@gmail.com>
2021-06-02 13:46:02 -0400

    starting more crud practice

---
+++ practice/db.php
@@ -0,0 +1,10 @@
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'favorite_colors');
+
+if(!$connection) {
+    die("database is not connected");
+} else {
+    echo "database is connected!";
+}
+
+?>
---
+++ practice/submitData.php
@@ -0,0 +1,27 @@
+<?php
+include "functions.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="submitData.php" method="post">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="color">Color</label>
+            <input type="text" name="color">
+        </div>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
