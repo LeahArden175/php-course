67540ce43818241993382abc39290c2fabaa1ee9
Leah Arden <leaharden175@gmail.com>
2021-06-08 15:10:47 -0400

    finished class practice

--- practice/class.php
+++ practice/class.php
@@ -1,6 +1,21 @@
 <?php
 
 
+class Dog {
+    var $color = "brown";
+    var $type = "lab";
+    var $age = "5";
+
+    function showAll() {
+        echo $this->color . "<br>";
+        echo $this->type . "<br>";
+        echo $this->age . "<br>";
+    }
+}
+
+$puppy = new Dog();
+$puppy->showAll();
+
 
 ?>
 
