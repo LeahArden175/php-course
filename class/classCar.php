72ab66faca661893d3febb9b4231aeceb38f9780
Leah Arden <leaharden175@gmail.com>
2021-06-08 14:42:18 -0400

    working on inheritance

--- class/classCar.php
+++ class/classCar.php
@@ -1,5 +1,33 @@
 <?php
 
+class Car{  //classes MUST be capitalized!
 
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 2;
+
+
+    function move() {
+        echo "the car is moving";
+    }
+
+} 
+
+// if(class_exists("car")) {
+ 
+//     echo "the class exists" . "<br>";
+// }
+
+// if(method_exists("Car", "move")) {
+//     echo "the method exists";
+// }
+
+$bmw = new Car();
+$bmw->move();
+echo "<br>";
+
+echo "<br>";
+echo $bmw->wheels;
 
 ?>
