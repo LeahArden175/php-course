7aad7448dcf6a45bda88e1bce2205b1cc1b158d6
Leah Arden <leaharden175@gmail.com>
2021-06-08 13:10:14 -0400

    basic post done;

--- http/post.php
+++ http/post.php
@@ -1,3 +1,11 @@
+<?php
+
+echo $_POST['name'];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,11 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="post.php" method="post">
+    <input type="text" name="name">
+    <button type="submit">Submit</button>
+</form>
     
 </body>
 </html>
