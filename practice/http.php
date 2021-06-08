232e7a3ec1b6820d7d9044033fa745e35768430a
Leah Arden <leaharden175@gmail.com>
2021-06-08 14:24:01 -0400

    finished http practice

--- practice/http.php
+++ practice/http.php
@@ -1,5 +1,18 @@
 <?php
 
+session_start();
+
+$_SESSION['greeting'] = "Welcome";
+
+echo $_SESSION['greeting'];
+
+print_r($_GET);
+
+$name = "Username";
+$value = "dodo123";
+$exipration = time() + (60*60*24*7);
+
+setcookie($name, $value, $exipration);
 
 ?>
 
@@ -11,6 +24,21 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+ $id = "hello";
+
+ if(isset($_COOKIE['Username'])) {
+     $user = $_COOKIE["Username"];
+
+     echo "<br>" . "the user is " . $user;
+ } else {
+     $user = 'nothing';
+ }
+
+?>
+
+<a href="http.php?id=<?php echo $id;?>">Click Here</a>
     
 </body>
 </html>
